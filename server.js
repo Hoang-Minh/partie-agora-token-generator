ce3be5bd0edcdd10462fd943b80f6bd9955fc7bf
Hoang-Minh <quataoxanh83@gmail.com>
2021-11-12 14:49:32 -0800

    Added cors support for get rtc token

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 const express = require("express");
-const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
+const cors = require("cors");
+
+const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
 const app = express();
 
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();    
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 // routes
-app.get("/api/agora/rtcToken", (req, res) => {
+app.get("/api/agora/rtcToken", cors(), (req, res) => {
+  const currentTimeStamp = Math.floor(Date.now() / 1000);
+  // token expire time, hardcode to 3600 seconds = 1 hour
+  const privilegeExpiredTs =
+    currentTimeStamp + Number(process.env.EXPIRATION_TIME_IN_SECONDS);
+  const { channelName } = req.query;
 
-    const currentTimeStamp = Math.floor(Date.now() / 1000);
-    // token expire time, hardcode to 3600 seconds = 1 hour
-    const privilegeExpiredTs = currentTimeStamp + Number(process.env.EXPIRATION_TIME_IN_SECONDS);    
-    const { channelName } = req.query;
+  if (!channelName) {
+    return res.status(400).json({ error: "Channel name is required" }).send();
+  }
 
-    if (!channelName) {
+  const key = RtcTokenBuilder.buildTokenWithUid(
+    app_id,
+    app_certificate,
+    channelName,
+    "0",
+    RtcRole.PUBLISHER,
+    privilegeExpiredTs
+  );
+  return res.json({ key: key }).send();
+});
 
-        return res.status(400).json({ "error": "Channel name is required" }).send();
-    }
-
-    const key = RtcTokenBuilder.buildTokenWithUid(process.env.API_ID, process.env.API_CERTIFICATE, channelName, 0, 0, privilegeExpiredTs);
-    return res.json({ "key": key }).send();
-})
-
-app.listen(process.env.PORT, () => console.log(`server is up at port at ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`server is up at port at ${process.env.PORT}`)
+);
