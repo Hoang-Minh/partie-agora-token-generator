62634ef465a6c0183d2f686435b85d25913dd8a8
Minh Nguyen <minh@partie.com>
2021-11-11 17:05:24 -0800

    try again

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const { port, app_id, app_certificate, expiration_time_in_seconds} = require("./config");
 const express = require("express");
 const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
 const app = express();
+
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();    
+}
 
 // routes
 app.get("/api/agora/rtcToken", (req, res) => {
 
     const currentTimeStamp = Math.floor(Date.now() / 1000);
     // token expire time, hardcode to 3600 seconds = 1 hour
-    const privilegeExpiredTs = currentTimeStamp + expiration_time_in_seconds;    
+    const privilegeExpiredTs = currentTimeStamp + process.env.EXPIRATION_TIME_IN_SECONDS;    
     const { channelName } = req.query;
 
     if (!channelName) {
@@ -16,8 +19,8 @@ app.get("/api/agora/rtcToken", (req, res) => {
         return res.status(400).json({ "error": "Channel name is required" }).send();
     }
 
-    const key = RtcTokenBuilder.buildTokenWithUid(app_id, app_certificate, channelName, "0", RtcRole.PUBLISHER, privilegeExpiredTs);
+    const key = RtcTokenBuilder.buildTokenWithUid(process.env.API_ID, process.env.API_CERTIFICATE, channelName, "0", RtcRole.PUBLISHER, privilegeExpiredTs);
     return res.json({ "key": key }).send();
 })
 
-app.listen(port, () => console.log(`server is up at port at ${port}`));
+app.listen(process.env.PORT, () => console.log(`server is up at port at ${process.env.PORT}`));
